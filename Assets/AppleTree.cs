2b392686c4112b0449b0329f1172c38ccfd69dfc
korsunator <35782970+korsunator@users.noreply.github.com>
2020-02-07 13:41:45 -0500

    added DropApple

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -12,9 +12,16 @@ public class AppleTree : MonoBehaviour {
     public float secondsBetweenAppleDrops = 1f;
 
 	void Start () {
-		
+        Invoke("DropApple", 2f);
 	}
 	
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
 	void Update () {
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
